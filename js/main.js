772b65ff0dc9ce29867b8fbf5cf9d697decb7c76
sajankafle26 <64882150+sajankafle26@users.noreply.github.com>
2022-04-05 15:52:11 +0545

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$('.owl-theme').owlCarousel({
+    loop:true,
+    margin:10,
+    dots:false,
+    nav:true,
+    mouseDrag:false,
+    autoplay:true,
+    animateOut: 'slideOutUp',
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
+
+$(".two").owlCarousel({
+    margin: 20,
+});
